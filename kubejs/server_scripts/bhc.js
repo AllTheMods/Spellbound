5e722bdf1ab3996fc48648ab5f0b002a2bcc042c
Traverse-Joe <roxxanrebirth@gmail.com>
2021-06-02 18:17:26 -0700

    cleaned up a bit

--- kubejs/server_scripts/bhc.js
+++ kubejs/server_scripts/bhc.js
@@ -12,8 +12,17 @@ onEvent('recipes', e => {
 
     e.custom({
         type: 'evilcraft:blood_infuser',
-        item : Item.of('minecraft:rotten_flesh').toJson(),
-            fluid: Fluid.of('evilcraft:blood', 50000).toJson(),
+        item : Item.of('xreliquary:zombie_heart').toJson(),
+        fluid: Fluid.of('evilcraft:blood', 50000).toJson(),
+        result: Item.of('bhc:red_heart').toJson(),
+        duration: 1200,
+        xp: 20,
+        tier: 0
+    })
+    e.custom({
+        type: 'evilcraft:blood_infuser',
+        item : Item.of('eidolon:zombie_heart').toJson(),
+        fluid: Fluid.of('evilcraft:blood', 25000).toJson(),
         result: Item.of('bhc:red_heart').toJson(),
         duration: 1200,
         xp: 20,
